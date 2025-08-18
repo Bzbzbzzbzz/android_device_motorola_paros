834690be46610d0dafce1dd46d24f5795bf36f51
felipeskyy <costafelipeew@gmail.com>
2025-08-18 06:22:27 +0000

    paros: Declare sensors hal config and move it to vendor

--- device.mk
+++ device.mk
@@ -86,6 +86,10 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Vibrator
 $(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.moto_sm8475-richtap)
 $(call soong_config_set,qti_vibrator,use_effect_stream,true)
